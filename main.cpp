b6551b82fe8835fe9c00be31f2041c8b9fab3154
Sukru Uzun <suk.uzun03@gmail.com>
2019-02-09 16:06:24 +0300

    Simple line chart is written and serial port config is done

--- main.cpp
+++ main.cpp
@@ -1,11 +1,77 @@
-#include "mainwindow.h"
-#include <QApplication>
+/*#include <QtWidgets/QApplication>
+#include <QtWidgets/QMainWindow>
+#include <QtCharts/QChartView>
+#include <QtCharts/QLineSeries>
+
+QT_CHARTS_USE_NAMESPACE
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+
+    QLineSeries *series = new QLineSeries();
+
+    series->append(0, 6);
+    series->append(2, 4);
+    series->append(3, 8);
+    series->append(7, 4);
+    series->append(10, 5);
+    *series << QPointF(11, 1) << QPointF(13, 3) << QPointF(17, 6) << QPointF(18, 3) << QPointF(20, 2);
+
+    QChart *chart = new QChart();
+    chart->legend()->hide();
+    chart->addSeries(series);
+    chart->createDefaultAxes();
+    chart->setTitle("Simple line chart example");
+
+    QChartView *chartView = new QChartView(chart);
+    chartView->setRenderHint(QPainter::Antialiasing);
+
+    QMainWindow window;
+    window.setCentralWidget(chartView);
+    window.resize(400, 300);
+    window.show();
+
+    return a.exec();
+}
+*/
+
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QMainWindow>
+#include <QtCharts/QChartView>
+#include <QtCharts/QSplineSeries>
+
+QT_CHARTS_USE_NAMESPACE
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    QSplineSeries *series = new QSplineSeries();
+    series->setName("spline");
+
+    series->append(0, 6);
+    series->append(2, 4);
+    series->append(3, 8);
+    series->append(7, 4);
+    series->append(10, 5);
+    *series << QPointF(11, 1) << QPointF(13, -2) << QPointF(17, 6) << QPointF(18, 3) << QPointF(20, 2);
+
+    QChart *chart = new QChart();
+    chart->legend()->hide();        // spline yazısını gizliyor
+    chart->addSeries(series);
+    chart->setTitle("Simple spline chart example");
+    chart->createDefaultAxes();
+    chart->axes(Qt::Vertical).first()->setRange(-5, 10);
+    chart->axes(Qt::Horizontal).first()->setRange(-10, 20);
+
+    QChartView *chartView = new QChartView(chart);
+    chartView->setRenderHint(QPainter::Antialiasing);
+
+    QMainWindow window;
+    window.setCentralWidget(chartView);
+    window.resize(400, 300);
+    window.show();
 
     return a.exec();
 }
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,26 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QSerialPort>
+
+QSerialPort static serial;
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    serial.setPortName("/dev/ttyUSB0");
+    serial.setBaudRate(QSerialPort::Baud9600);
+    serial.setDataBits(QSerialPort::Data8);
+    serial.setParity(QSerialPort::NoParity);
+    serial.setStopBits(QSerialPort::OneStop);
+    serial.setFlowControl(QSerialPort::NoFlowControl);
+    serial.open(QIODevice::ReadWrite);
+    serial.write("Selam");
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+    serial.close();
 }
