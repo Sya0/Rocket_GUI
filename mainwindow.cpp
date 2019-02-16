ada6647d0a3a7ffc08f5a042dd1f8a1b675f48f4
Sukru Uzun <suk.uzun03@gmail.com>
2019-02-16 10:19:36 +0300

    Serial port parameters are made portable

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,25 +1,57 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-#include <QSerialPort>
-
-QSerialPort serial;
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    serial.setPortName("/dev/ttyS1");
-    serial.setBaudRate(QSerialPort::Baud9600);
-    serial.setDataBits(QSerialPort::Data8);
-    serial.setParity(QSerialPort::NoParity);
-    serial.setStopBits(QSerialPort::OneStop);
-    serial.setFlowControl(QSerialPort::NoFlowControl);
-    serial.open(QIODevice::ReadWrite);
+
+    stm_available = false;
+    stm_port_name = "";
+    serial = new QSerialPort;
+
+  /*  qDebug() << "Number of available ports: " << QSerialPortInfo::availablePorts().length();
+    foreach(const QSerialPortInfo &serialPortInfo, QSerialPortInfo::availablePorts()){
+        qDebug() << "has Vendor ID: " << serialPortInfo.hasVendorIdentifier();
+        if(serialPortInfo.hasVendorIdentifier()){
+            qDebug() << "Vendor ID: " << serialPortInfo.vendorIdentifier();
+        }
+        qDebug() << "has Product ID: " << serialPortInfo.hasProductIdentifier();
+        if(serialPortInfo.hasProductIdentifier()){
+            qDebug() << "Product ID: " << serialPortInfo.productIdentifier();
+        }
+    }*/
+
+    foreach(const QSerialPortInfo &serialPortInfo, QSerialPortInfo::availablePorts()){
+        if(serialPortInfo.hasVendorIdentifier() && serialPortInfo.hasProductIdentifier()){
+            if((serialPortInfo.vendorIdentifier() == stm_vendor_id) &&
+                    (serialPortInfo.productIdentifier() == stm_product_id)){
+                stm_port_name = serialPortInfo.portName();
+                stm_available = true;
+                qDebug() << serialPortInfo.portName();
+            }
+        }
+    }
+
+    if(stm_available){
+        serial->setPortName("/dev/ttyUSB0");
+        serial->open(QIODevice::ReadWrite);
+        serial->setBaudRate(QSerialPort::Baud9600);
+        serial->setDataBits(QSerialPort::Data8);
+        serial->setParity(QSerialPort::NoParity);
+        serial->setStopBits(QSerialPort::OneStop);
+        serial->setFlowControl(QSerialPort::NoFlowControl);
+        serial->write("Selam");
+    }
+    else{
+        QMessageBox::warning(this, "Port error", "Couldn't find the port");
+    }
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
-    serial.close();
+    serial->close();
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,10 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QSerialPort>
+#include <QSerialPortInfo>
+#include <QDebug>
+#include <QtWidgets>
 
 namespace Ui {
 class MainWindow;
@@ -17,6 +21,12 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    QSerialPort *serial = nullptr;
+    // Alt kısmı doldur
+    static const quint16 stm_vendor_id = 1659;
+    static const quint16 stm_product_id = 8963;
+    QString stm_port_name;
+    bool stm_available;
 };
 
 #endif // MAINWINDOW_H
